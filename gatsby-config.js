3e6e68a48b42cb00732fe5b3cea8db083525fcfe
davidlunadeleon <davidlunadeleon@gmail.com>
2020-09-04 20:50:54 -0500

    Add dependencies and configs
    - Add gatsby-transformer-remark to dependencies
    - Add source for blogposts in gatsby config
    - Fix minor typo in header.module.css

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,11 +9,19 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
+        name: `blogPosts`,
+        path: `${__dirname}/blogPosts`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
     },
     `gatsby-transformer-sharp`,
+    `gatsby-transformer-remark`,
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
