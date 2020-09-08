ed877b739cb144d0865bcbe87f03a76eaa8d7477
davidlunadeleon <davidlunadeleon@gmail.com>
2020-09-07 20:39:03 -0500

    Modify navbar
    - Fix styles importing from css module
    - Add common links
    - Add searchbar (functionality pending)
    - Make collapsible and more mobile friendly

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,35 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
-import { Navbar } from "react-bootstrap"
+import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap"
 
-import "./header.module.css"
+import styles from "./header.module.css"
 
 const Header = ({ siteTitle }) => (
-  <Navbar bg="dark" variant="dark">
+  <Navbar bg="dark" variant="dark" collapseOnSelect expand="lg">
     <Navbar.Brand>
       <Link className="navbar-brand" to="/">
         {siteTitle}
       </Link>
     </Navbar.Brand>
+    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+    <Navbar.Collapse id="responsive-navbar-nav">
+      <Nav className="mr-auto">
+        <Link className="nav-link" to="/">
+          Home
+        </Link>
+        <Link className="nav-link" to="/posts">
+          Posts
+        </Link>
+        <Link className="nav-link" to="/about-me">
+          About Me
+        </Link>
+      </Nav>
+      <Form inline className={styles.navbarInlineForm}>
+        <FormControl type="text" placeholder="Search..." className="mr-sm-2" />
+        <Button variant="outline-info">Search</Button>
+      </Form>
+    </Navbar.Collapse>
   </Navbar>
 )
 
