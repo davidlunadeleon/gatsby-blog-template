c41b87c6f6e6e73324dc890778cee8170d330556
davidlunadeleon <davidlunadeleon@gmail.com>
2020-09-12 17:44:00 -0500

    Add posts to home page
    - Add query for the index file to get most recent posts
    - Modify posts component to return list of recent posts

--- src/components/posts.js
+++ src/components/posts.js
@@ -1,7 +1,23 @@
 import React from "react";
 
 const Posts = ({ posts }) => {
-	return <div></div>;
+	const makePostList = () => {
+		return posts.map((postNode) => {
+			return (
+				<div key={postNode.node.id}>
+					<h3>{postNode.node.frontmatter.title}</h3>
+					<p>
+						Date: {postNode.node.frontmatter.date}
+						{". "}
+						Time to read: {postNode.node.timeToRead} minutes
+					</p>
+					<p>{postNode.node.excerpt}</p>
+				</div>
+			);
+		});
+	};
+
+	return <div>{makePostList()}</div>;
 };
 
 export default Posts;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,39 @@
 import React from "react";
-import { Link } from "gatsby";
+import { graphql } from "gatsby";
 
 import Layout from "../components/layout";
-import Image from "../components/image";
 import SEO from "../components/seo";
+import Posts from "../components/posts";
 
-const IndexPage = () => (
-	<Layout>
-		<SEO title="Home" />
-		<h1>Hi people</h1>
-		<p>Welcome to your new Gatsby site.</p>
-		<p>Now go build something great.</p>
-		<div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-			<Image />
-		</div>
-		<Link to="/page-2/">Go to page 2</Link> <br />
-		<Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-	</Layout>
-);
+const IndexPage = ({ data }) => {
+	return (
+		<Layout>
+			<SEO title="Home" />
+			<h1>Recent posts</h1>
+			<Posts posts={data.allMarkdownRemark.edges} />
+		</Layout>
+	);
+};
+
+export const query = graphql`
+	query {
+		allMarkdownRemark(
+			limit: 10
+			sort: { fields: frontmatter___date, order: DESC }
+		) {
+			edges {
+				node {
+					frontmatter {
+						title
+						date(formatString: "DD MMMM, YYYY")
+					}
+					excerpt
+					id
+					timeToRead
+				}
+			}
+		}
+	}
+`;
 
 export default IndexPage;
